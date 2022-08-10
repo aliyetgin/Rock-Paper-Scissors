e7e12f70a82abc95d8dd32cd76a01b495ed9d005
Ali Yetgin <yetgin.ali@outlook.com>
2022-08-10 18:04:13 +0300

    Do javascript side of project

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const buttons = document.querySelectorAll('button');
+buttons.forEach(btn => btn.addEventListener("click", playRound)); 
+
 function getComputerChoice() {
     let random =  Math.floor(Math.random() * 3);
     if(random === 0) return 'rock';
@@ -5,53 +8,79 @@ function getComputerChoice() {
     else return 'scissors';
 }
 
-let getUserChoice = () => window.prompt("Select one of rock, paper, scissors").toLowerCase();
-
 let playerWinCount = 0; 
-let computerWinCount = 0;
+let computerWinCount = 0; 
 
-function playRound(playerSelection, computerSelection){
-    
-    if(playerSelection === computerSelection)  alert(`tie game score = player: ${playerWinCount} computer: ${computerWinCount}`);
-    
+const div = document.getElementById("display-div");
+
+function playRound(event){
+    div.textContent = "";
+    let playerSelection = event.target.textContent.toLowerCase();
+    const computerSelection = getComputerChoice();
+    console.log(playerSelection);
+   
+    const result = document.createElement("p");
+    if(playerSelection === computerSelection)  {
+        result.textContent = `tie game`;
+        div.appendChild(result);
+    }
     
     if(playerSelection === "rock"){
          if(computerSelection === "paper") {
+            result.textContent = `you lose! paper beats rock`;
+            div.appendChild(result);
             computerWinCount++;
-            alert(`you lose! paper beats rock score = player: ${playerWinCount} computer: ${computerWinCount}`);
         } else if(computerSelection === "scissors"){
+            result.textContent = `you win! rock beats scissors`;
+            div.appendChild(result);
             playerWinCount++;
-            alert(`you win! rock beats scissors score = player: ${playerWinCount} computer: ${computerWinCount}`);
         }
     }
 
     if(playerSelection === "paper"){
         if(computerSelection === "rock" ) {
+            result.textContent = `you win! paper beats rock`;
+            div.appendChild(result);
             playerWinCount++;
-            alert(`you win! paper beats rock score = player: ${playerWinCount} computer: ${computerWinCount}`);
         } else if(computerSelection === "scissors"){ 
+            result.textContent = `you lose! scissors beats paper`;
+            div.appendChild(result);
             computerWinCount++;
-            alert(`you lose! scissors beats paper score = player: ${playerWinCount} computer: ${computerWinCount}`);
         }
     }
 
     if(playerSelection === "scissors"){
         if(computerSelection === "rock"){
+            result.textContent = `you lose! rock beats scissors`;
+            div.appendChild(result);
             computerWinCount++;
-            alert(`you lose! rock beats scissors score = player: ${playerWinCount} computer: ${computerWinCount}`);
         } 
         else if(computerSelection === "paper"){
+            result.textContent = `you win! scissors beats paper`;
+            div.appendChild(result);
             playerWinCount++;
-            alert(`you win! scissors beats paperscore = player: ${playerWinCount} computer: ${computerWinCount}`);
         }
     }
+    isOver();
+    displayScore();
 }
 
-function playGame(){ 
-    while (playerWinCount != 3 && computerWinCount != 3) {
-        const playerSelection = getUserChoice();
-        const computerSelection = getComputerChoice();
-        playRound(playerSelection, computerSelection);
+const sc_div = document.getElementById("score-div");
+const score = document.createElement("p");
+
+function displayScore(){
+  score.textContent = `Score: you = ${playerWinCount} vs computer ${computerWinCount}`;
+  sc_div.textConten = "";
+  sc_div.appendChild(score);
+}
+
+function isOver(){
+    if(playerWinCount === 5 ||  computerWinCount === 5){
+      buttons.forEach(btn => btn.removeEventListener("click", playRound));
+
+      const winner = playerWinCount === 5 ? "you" : "computer";
+      const announcement = document.createElement("p");
+      announcement.textContent = `${winner} won!!`;
+      div.appendChild(announcement);
     }
 }
-playGame();
